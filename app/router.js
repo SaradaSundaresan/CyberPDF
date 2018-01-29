d86d961a38b425dd6fb72d75fdd51681e3ec1506
Charlie So <cso@revenueanalytics.com>
2018-01-29 15:38:59 -0500

    Adding route to the files.  Possibly missing.:

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('homepage');
+  this.route('contact');
+  this.route('faq');
+  this.route('bugs');
 });
 
 export default Router;
